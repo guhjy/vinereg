9a6ddee1406a12de246c667d4a3aba2c0920ffa2
tnagler <thomas.nagler@tum.de>
2017-10-06 00:05:12 +0200

    try onepar

--- R/vinereg.R
+++ R/vinereg.R
@@ -35,7 +35,7 @@
 #' abalone.f <- abalone[abalone$sex=="M", -1]
 #' abalone.f <- abalone.f[-which.max(abalone.f$height), ]
 #' vinereg(whole ~ length + diameter + height, data = abalone.f,
-#'         family_set = "parametric", correction = "AIC")
+#'         family_set = "onepar", correction = "AIC")
 #'
 #'
 #' @seealso \code{\link{predict.vinereg}}
